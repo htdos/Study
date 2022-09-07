7f4c66df5aa9dd41b951483d63bc7207a109b1b3
htdos <soooyeon1999@gmail.com>
2022-09-07 13:58:54 +0900

    Add baekjoon file

---
+++ 단계별 문제/02.조건문/Bj2480.java
@@ -0,0 +1,27 @@
+package again1;
+import java.util.Scanner;
+
+public class Bj2480 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a,b,c;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		
+		if(a==b && a==c && b==c) {
+			System.out.print(10000 + (a * 1000));
+		}else if((a==b) || (a==c)) {
+			System.out.print(1000 + (a * 100));
+		}else if(b==c) {
+			System.out.print(1000+(b*100));
+		
+		}else {
+			System.out.print((Math.max(a, Math.max(b, c)))*100);
+		}
+
+	}
+
+}
