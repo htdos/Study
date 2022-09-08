8826a99ae399777c3e0dbe4396b9766da74843e5
htdos <soooyeon1999@gmail.com>
2022-09-08 14:33:17 +0900

    Add baekjoon file

---
+++ 단계별 문제/03.반복문/Bj2739.java
@@ -0,0 +1,17 @@
+package again1;
+import java.util.Scanner;
+
+public class Bj2739 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		for(int i=1; i<=9; i++) {
+		System.out.println(a+" * "+ i +" = "+a*i);
+		}
+
+	}
+
+}
