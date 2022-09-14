f1d770253d5b4a0327041682edf8e8f9d9171d3b
htdos <soooyeon1999@gmail.com>
2022-09-14 14:22:34 +0900

    Add baekjoon file

---
+++ 단계별 문제/03.반복문/Bj2438.java
@@ -0,0 +1,18 @@
+package again1;
+import java.util.Scanner;
+public class Bj2438 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=i; j++) {
+			System.out.print("*");
+		}
+			System.out.println();
+		}
+	}
+
+}
