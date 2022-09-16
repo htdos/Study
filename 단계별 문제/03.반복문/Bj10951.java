e6a2e7ba83d0b74d3eec3fa1e8a557c144d74c64
htdos <soooyeon1999@gmail.com>
2022-09-16 17:31:32 +0900

    Add baekjoon file

---
+++ 단계별 문제/03.반복문/Bj10951.java
@@ -0,0 +1,15 @@
+package again1;
+import java.util.Scanner;
+public class Bj10951 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	
+		while(sc.hasNextInt()) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a+b);
+		}
+	}
+
+}
