c21fceaa03f0e6e40d11744afe4976eabbfb8843
htdos <soooyeon1999@gmail.com>
2022-09-17 16:46:31 +0900

    Add baekjoon file

---
+++ 단계별 문제/03.반복문/Bj10871.java
@@ -0,0 +1,26 @@
+package again1;
+import java.util.Scanner;
+
+public class Bj10871 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n,x;
+		n = sc.nextInt();
+		x = sc.nextInt();
+		
+		int a[] = new int[n];
+				
+		for(int i=0; i<n; i++) {
+			a[i] = sc.nextInt();
+		}
+		
+		for(int i=0; i<n; i++) {
+			if(a[i]<x) {
+				System.out.print(a[i]+" ");
+			}
+		}
+	}
+
+}
